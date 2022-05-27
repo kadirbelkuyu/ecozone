5e9e4e5b3d783d8e83af1226cb05f9849aa7da22
kadirbelkuyu <kadirbelkuyu@gmail.com>
2022-05-27 20:52:25 +0300

    flask import

---
+++ app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+
+DEVELOPMENT_ENV  = True
+
+app = Flask(__name__)
+
+app_data = {
+    "name":         "ecozone",
+    "description":  "",
+    "author":       "ecozone Team",
+    "html_title":   "",
+    "project_name": "",
+    "keywords":     "ecozone"
+}
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', app_data=app_data)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', app_data=app_data)
+
+
+@app.route('/service')
+def service():
+    return render_template('service.html', app_data=app_data)
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', app_data=app_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=DEVELOPMENT_ENV)
